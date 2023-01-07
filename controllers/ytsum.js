389278537e627f2aed2ec08535a9d7259a97526e
kunbeg <goodkunal723@gmail.com>
2023-01-07 12:52:06 +0530

    commit

--- controllers/ytsum.js
+++ controllers/ytsum.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const { Youtube } = require("tube-api");
 const Jabber = require("jabber");
-const {response} = require('./test')
+const { response } = require("./test");
 exports.getSummary = async (req, res) => {
   try {
     console.log(req.body);
@@ -10,11 +10,16 @@ exports.getSummary = async (req, res) => {
     const videoId = url.split("=")[1];
     const video = await ytube.videoInfo(videoId);
     const jabber = new Jabber();
-    
-    setTimeout(()=>{
-      const arr = response.map(obj=>{
-        return {...obj,words:Math.floor(Math.random()*200)}
-      })
+
+    setTimeout(() => {
+      const arr = response.map((obj) => {
+        return {
+          ...obj,
+          charLen: obj.summary.length,
+          execution_time: Math.floor(obj.execution_time * 1000) / 1000,
+          similarity_score: Math.floor(obj.similarity_score * 1000) / 1000,
+        };
+      });
       const obj = {
         url: url,
         title: video.title,
@@ -23,8 +28,7 @@ exports.getSummary = async (req, res) => {
         summary: arr,
       };
       res.status(200).json(obj);
-    },6000)
-    
+    }, 6000);
   } catch (err) {
     res.status(400).json(err.message);
   }
