c7efd3400502f8bac5d07f927c89f818c00d08d0
kunbeg <goodkunal723@gmail.com>
2023-01-28 08:08:58 +0530

    commit

--- controllers/ytsum.js
+++ controllers/ytsum.js
@@ -10,25 +10,46 @@ exports.getSummary = async (req, res) => {
     const videoId = url.split("=")[1];
     const video = await ytube.videoInfo(videoId);
     const jabber = new Jabber();
+    // console.log(video);
 
-    setTimeout(() => {
-      const arr = response.map((obj) => {
-        return {
-          ...obj,
-          charLen: obj.summary.length,
-          execution_time: Math.floor(obj.execution_time * 1000) / 1000,
-          similarity_score: Math.floor(obj.similarity_score * 1000) / 1000,
+    fetch(`http://127.0.0.1:8000/summarize/${url.replace('watch?','')}`)
+      .then(res => res.json())
+      .then(data => {
+        const arr = data.map((obj) => {
+          return {
+            ...obj,
+            charLen: obj.summary.length,
+            execution_time: Math.floor(obj.execution_time * 1000) / 1000,
+            similarity_score: Math.floor(obj.similarity_score * 1000) / 1000,
+          };
+        });
+        const obj = {
+          url: url,
+          title: video.title,
+          pic: video.thumbnails.standard.url,
+          type: 0,
+          summary: arr,
         };
-      });
-      const obj = {
-        url: url,
-        title: video.title,
-        pic: video.thumbnails.default.url,
-        type: 0,
-        summary: arr,
-      };
-      res.status(200).json(obj);
-    }, 6000);
+        res.status(200).json(obj);
+      })
+    // setTimeout(() => {
+    //   const arr = response.map((obj) => {
+    //     return {
+    //       ...obj,
+    //       charLen: obj.summary.length,
+    //       execution_time: Math.floor(obj.execution_time * 1000) / 1000,
+    //       similarity_score: Math.floor(obj.similarity_score * 1000) / 1000,
+    //     };
+    //   });
+    //   const obj = {
+    //     url: url,
+    //     title: video.title,
+    //     pic: video.thumbnails.standard.url,
+    //     type: 0,
+    //     summary: arr,
+    //   };
+    //   res.status(200).json(obj);
+    // }, 6000);
   } catch (err) {
     res.status(400).json(err.message);
   }
